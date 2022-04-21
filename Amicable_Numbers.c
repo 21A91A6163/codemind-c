3d92cd568de2e5f21995ba7ed6cc790877cefe8d
21A91A6163 <97424651+21A91A6163@users.noreply.github.com>
2022-04-21 14:40:38 +0530

    #include<stdio.h> int main() {     int m,n,sum1=0,sum2=0,i;     scanf("%d%d",&m,&n);     for(i=1;i<=m/2;i++)     {     	if(m%i==0)     	{     		sum1=sum1+i; 		} 	} 	for(i=1;i<=n/2;i++)     {     	if(n%i==0)     	{     		sum2=sum2+i; 		} 	}     if(m==sum2 && n==sum1)     {         printf("Amicable");     }     else     {         printf("Not Amicable");     } }

---
+++ Amicable_Numbers.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+    int m,n,sum1=0,sum2=0,i;
+    scanf("%d%d",&m,&n);
+    for(i=1;i<=m/2;i++)
+    {
+    	if(m%i==0)
+    	{
+    		sum1=sum1+i;
+		}
+	}
+	for(i=1;i<=n/2;i++)
+    {
+    	if(n%i==0)
+    	{
+    		sum2=sum2+i;
+		}
+	}
+    if(m==sum2 && n==sum1)
+    {
+        printf("Amicable");
+    }
+    else
+    {
+        printf("Not Amicable");
+    }
+}
