b00e3efd09e833b3e836ca4d11f25dea23069308
21A91A6163 <97424651+21A91A6163@users.noreply.github.com>
2022-04-23 20:53:49 +0530

    #include<stdio.h> #include<math.h> int main() {     int n,arr[100],i,sum=0,even;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(arr[i]%2==0)         {             even=i;         }     }      printf("%d",even); }

---
+++ Last_even_element_index.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int n,arr[100],i,sum=0,even;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]%2==0)
+        {
+            even=i;
+        }
+    }
+     printf("%d",even); 
+}
