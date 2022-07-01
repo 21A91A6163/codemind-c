daf9b18cccc967a9b4573bae4fca5d6a569358f7
21A91A6163 <97424651+21A91A6163@users.noreply.github.com>
2022-07-01 21:16:14 +0530

    #include<stdio.h> int main() {     char str[100];     int i,c=0;     scanf("%[^ ]s",str);     for(i=0;str[i]!=NULL;i++)     {         if(str[i]==' ')         {             c++;         }     }     printf("%d",c+1); }

--- words_in_a_string.c
+++ words_in_a_string.c
@@ -2,7 +2,7 @@
 int main()
 {
     char str[100];
-    int c=0,i;
+    int i,c=0;
     scanf("%[^
 ]s",str);
     for(i=0;str[i]!=NULL;i++)
@@ -13,5 +13,4 @@ int main()
         }
     }
     printf("%d",c+1);
-    
 }
